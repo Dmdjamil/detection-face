1f2ca47a83c629a9f33a9ae85f0c299bc403dafc
Dmdjamil <moustaphadiome@gmail.com>
2026-04-02 18:55:00 +0000

    detectimg.py

--- detectimg.py
+++ detectimg.py
@@ -1,6 +1,16 @@
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+import cv2
+from streamlit_webrtc import VideoTransformerBase
 
-st.title("Webcam test")
+class FaceDetector(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
 
-webrtc_streamer(key="example")
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = cv2.CascadeClassifier(
+            cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+        ).detectMultiScale(gray, 1.1, 4)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+
+        return img
