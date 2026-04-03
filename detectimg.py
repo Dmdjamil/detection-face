76315daa3361f5d37793317fd40401cc18c35ff2
Dmdjamil <moustaphadiome@gmail.com>
2026-04-03 03:43:55 +0000

    detectimg.py

--- detectimg.py
+++ detectimg.py
@@ -1,5 +1,10 @@
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 import cv2
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+
+# Initialisation du classifieur de visages
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 
 class FaceDetector(VideoTransformerBase):
     def transform(self, frame):
@@ -10,8 +15,17 @@ class FaceDetector(VideoTransformerBase):
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         return img
 
-webrtc_streamer(key="face", video_transformer_factory=FaceDetector)
+# Ajout de la configuration STUN pour le déploiement
+webrtc_streamer(
+    key="face",
+    video_transformer_factory=FaceDetector,
+    rtc_configuration={
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]}
+        ]
+    }
+)
